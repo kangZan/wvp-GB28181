a299ef477eb3c6118e88dfa491e36b67bfed77ab
kangzan <kangzan@spddw.com>
2020-09-24 15:00:09 +0800

    查询预置位

--- src/main/java/com/genersoft/iot/vmp/gb28181/transmit/callback/DeferredResultHolder.java
+++ src/main/java/com/genersoft/iot/vmp/gb28181/transmit/callback/DeferredResultHolder.java
@@ -8,35 +8,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.async.DeferredResult;
 
-/**    
- * @Description:TODO(这里用一句话描述这个类的作用)   
+/**
+ * @Description:TODO(这里用一句话描述这个类的作用)
  * @author: swwheihei
- * @date:   2020年5月8日 下午7:59:05     
+ * @date: 2020年5月8日 下午7:59:05
  */
 @Component
 public class DeferredResultHolder {
-	
-	public static final String CALLBACK_CMD_DEVICEINFO = "CALLBACK_DEVICEINFO";
-	
-	public static final String CALLBACK_CMD_CATALOG = "CALLBACK_CATALOG";
-	
-	public static final String CALLBACK_CMD_RECORDINFO = "CALLBACK_RECORDINFO";
 
-	private Map<String, DeferredResult> map = new HashMap<String, DeferredResult>();
-	
-	public void put(String key, DeferredResult result) {
-		map.put(key, result);
-	}
-	
-	public DeferredResult get(String key) {
-		return map.get(key);
-	}
-	
-	public void invokeResult(RequestMessage msg) {
-		DeferredResult result = map.get(msg.getId());
-		if (result == null) {
-			return;
-		}
-		result.setResult(new ResponseEntity<>(msg.getData(),HttpStatus.OK));
-	}
+    public static final String CALLBACK_CMD_DEVICEINFO = "CALLBACK_DEVICEINFO";
+
+    public static final String CALLBACK_CMD_CATALOG = "CALLBACK_CATALOG";
+
+    public static final String CALLBACK_CMD_RECORDINFO = "CALLBACK_RECORDINFO";
+    public static final String CALLBACK_CMD_PRESETQUERY = "CALLBACK_PRESETQUERY";
+
+    private Map<String, DeferredResult> map = new HashMap<String, DeferredResult>();
+
+    public void put(String key, DeferredResult result) {
+        map.put(key, result);
+    }
+
+    public DeferredResult get(String key) {
+        return map.get(key);
+    }
+
+    public void invokeResult(RequestMessage msg) {
+        DeferredResult result = map.get(msg.getId());
+        if (result == null) {
+            return;
+        }
+        result.setResult(new ResponseEntity<>(msg.getData(), HttpStatus.OK));
+    }
 }
--- src/main/java/com/genersoft/iot/vmp/vmanager/device/DeviceController.java
+++ src/main/java/com/genersoft/iot/vmp/vmanager/device/DeviceController.java
@@ -26,75 +26,88 @@ import com.genersoft.iot.vmp.storager.IVideoManagerStorager;
 @RestController
 @RequestMapping("/api")
 public class DeviceController {
-	
-	private final static Logger logger = LoggerFactory.getLogger(DeviceController.class);
-	
-	@Autowired
-	private IVideoManagerStorager storager;
-	
-	@Autowired
-	private SIPCommander cmder;
-	
-	@Autowired
-	private DeferredResultHolder resultHolder;
-	
-	@Autowired
-	private DeviceOffLineDetector offLineDetector;
-	
-	@GetMapping("/devices/{deviceId}")
-	public ResponseEntity<Device> devices(@PathVariable String deviceId){
-		
-		if (logger.isDebugEnabled()) {
-			logger.debug("查询视频设备API调用，deviceId：" + deviceId);
-		}
-		
-		Device device = storager.queryVideoDevice(deviceId);
-		return new ResponseEntity<>(device,HttpStatus.OK);
-	}
-	
-	@GetMapping("/devices")
-	public ResponseEntity<List<Device>> devices(){
-		
-		if (logger.isDebugEnabled()) {
-			logger.debug("查询所有视频设备API调用");
-		}
-		
-		List<Device> deviceList = storager.queryVideoDeviceList(null);
-		return new ResponseEntity<>(deviceList,HttpStatus.OK);
-	}
-	
-	@PostMapping("/devices/{deviceId}/sync")
-	public DeferredResult<ResponseEntity<Device>> devicesSync(@PathVariable String deviceId){
-		
-		if (logger.isDebugEnabled()) {
-			logger.debug("设备信息同步API调用，deviceId：" + deviceId);
-		}
-		
-		Device device = storager.queryVideoDevice(deviceId);
+
+    private final static Logger logger = LoggerFactory.getLogger(DeviceController.class);
+
+    @Autowired
+    private IVideoManagerStorager storager;
+
+    @Autowired
+    private SIPCommander cmder;
+
+    @Autowired
+    private DeferredResultHolder resultHolder;
+
+    @Autowired
+    private DeviceOffLineDetector offLineDetector;
+
+    @GetMapping("/devices/{deviceId}")
+    public ResponseEntity<Device> devices(@PathVariable String deviceId) {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("查询视频设备API调用，deviceId：" + deviceId);
+        }
+
+        Device device = storager.queryVideoDevice(deviceId);
+        return new ResponseEntity<>(device, HttpStatus.OK);
+    }
+
+    @GetMapping("/devices")
+    public ResponseEntity<List<Device>> devices() {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("查询所有视频设备API调用");
+        }
+
+        List<Device> deviceList = storager.queryVideoDeviceList(null);
+        return new ResponseEntity<>(deviceList, HttpStatus.OK);
+    }
+
+    @PostMapping("/devices/{deviceId}/sync")
+    public DeferredResult<ResponseEntity<Device>> devicesSync(@PathVariable String deviceId) {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("设备信息同步API调用，deviceId：" + deviceId);
+        }
+
+        Device device = storager.queryVideoDevice(deviceId);
         cmder.catalogQuery(device);
         DeferredResult<ResponseEntity<Device>> result = new DeferredResult<ResponseEntity<Device>>();
-        resultHolder.put(DeferredResultHolder.CALLBACK_CMD_CATALOG+deviceId, result);
+        resultHolder.put(DeferredResultHolder.CALLBACK_CMD_CATALOG + deviceId, result);
         return result;
-	}
-	
-	@PostMapping("/devices/{deviceId}/delete")
-	public ResponseEntity<String> delete(@PathVariable String deviceId){
-		
-		if (logger.isDebugEnabled()) {
-			logger.debug("设备信息删除API调用，deviceId：" + deviceId);
-		}
-		
-		if (offLineDetector.isOnline(deviceId)) {
-			return new ResponseEntity<String>("不允许删除在线设备！", HttpStatus.NOT_ACCEPTABLE);
-		}
-		boolean isSuccess = storager.delete(deviceId);
-		if (isSuccess) {
-			JSONObject json = new JSONObject();
-			json.put("deviceId", deviceId);
-			return new ResponseEntity<>(json.toString(),HttpStatus.OK);
-		} else {
-			logger.warn("设备预览API调用失败！");
-			return new ResponseEntity<String>("设备预览API调用失败！", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+    }
+
+    @PostMapping("/presets/{deviceId}")
+    public DeferredResult<ResponseEntity<Device>> presetQuery(@PathVariable String deviceId) {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("查询预置位API调用，deviceId：" + deviceId);
+        }
+        Device device = storager.queryVideoDevice(deviceId);
+        cmder.presetQuery(device);
+        DeferredResult<ResponseEntity<Device>> result = new DeferredResult<ResponseEntity<Device>>();
+        resultHolder.put(DeferredResultHolder.CALLBACK_CMD_PRESETQUERY + deviceId, result);
+        return result;
+    }
+
+    @PostMapping("/devices/{deviceId}/delete")
+    public ResponseEntity<String> delete(@PathVariable String deviceId) {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("设备信息删除API调用，deviceId：" + deviceId);
+        }
+
+        if (offLineDetector.isOnline(deviceId)) {
+            return new ResponseEntity<String>("不允许删除在线设备！", HttpStatus.NOT_ACCEPTABLE);
+        }
+        boolean isSuccess = storager.delete(deviceId);
+        if (isSuccess) {
+            JSONObject json = new JSONObject();
+            json.put("deviceId", deviceId);
+            return new ResponseEntity<>(json.toString(), HttpStatus.OK);
+        } else {
+            logger.warn("设备预览API调用失败！");
+            return new ResponseEntity<String>("设备预览API调用失败！", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
