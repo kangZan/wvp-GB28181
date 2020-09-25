308c986c999a8ec0e5b9829dc4dd00dd9d120d7f
kangzan <kangzan@spddw.com>
2020-09-25 09:31:45 +0800

    add preset item bean

---
+++ src/main/java/com/genersoft/iot/vmp/gb28181/bean/PresetItem.java
@@ -0,0 +1,30 @@
+package com.genersoft.iot.vmp.gb28181.bean;
+
+/**
+ * @program: wvp
+ * @description: TODO
+ * @author: zan.kang
+ * @history: 1.created by zan.kang 2020/9/22 9:29
+ * 2.
+ */
+public class PresetItem {
+
+    private String PresetId;
+    private String PresetName;
+
+    public String getPresetId() {
+        return PresetId;
+    }
+
+    public void setPresetId(String presetId) {
+        PresetId = presetId;
+    }
+
+    public String getPresetName() {
+        return PresetName;
+    }
+
+    public void setPresetName(String presetName) {
+        PresetName = presetName;
+    }
+}
